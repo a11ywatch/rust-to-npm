c7275e45202c26841da7bf6eb43f8157f0a8bda2
j-mendez <jeff@a11ywatch.com>
2022-09-24 11:20:22 -0400

    chore(pre-install): fix npm install upgrading package

--- src/generators/pre_install.rs
+++ src/generators/pre_install.rs
@@ -1,24 +1,20 @@
 pub use super::generate_installs;
 
 /// create the pre-install script for the repo
-pub fn generate_pre_install(name: &String) -> String {
+pub fn generate_pre_install(name: &String, version: &String) -> String {
     format!(
         r#"{}
 const binp = path.join(cargoDir, "bin", "{name}");
 
-if (!fs.existsSync(binp)) {{
-  console.log("Installing and compiling {name}...");
-  exec(`cargo install {name}`, (error, stdout, stderr) => {{
-    console.log(stdout);
-    if (error || stderr) {{
-      console.log(error || stderr);
-    }} else {{
-      console.log("install finished!");
-    }}
-  }});
-}} else {{
-  console.log("{name} found install skipping!");
-}}
+console.log("Installing and compiling {name} {version}...");
+exec(`cargo install {name} --vers {version}`, (error, stdout, stderr) => {{
+  console.log(stdout);
+  if (error || stderr) {{
+    console.log(error || stderr);
+  }} else {{
+    console.log("install finished!");
+  }}
+}});
 
     "#,
         generate_installs()
