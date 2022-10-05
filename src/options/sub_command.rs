a539314a02ba2a4ae728db5052bbdb4bcd4c148a
j-mendez <jeff@a11ywatch.com>
2022-10-05 05:53:23 -0400

    feat(npm): add custom name package name ability

--- src/options/sub_command.rs
+++ src/options/sub_command.rs
@@ -3,7 +3,18 @@ use clap::Subcommand;
 #[derive(Subcommand)]
 pub enum Commands {
     /// Build the project for node.
-    BUILD {},
+    BUILD {
+        /// custom npm package name ex: @myorg/project
+        #[clap(short, long)]
+        npm_package_name: Option<String>
+    },
     /// Build and deploy the project to crates.io and npm.
-    DEPLOY {},
+    DEPLOY {
+        /// build the package for node
+        #[clap(short, long)]
+        build: bool,
+        /// custom npm package name ex: @myorg/project
+        #[clap(short, long)]
+        npm_package_name: Option<String>
+    },
 }
